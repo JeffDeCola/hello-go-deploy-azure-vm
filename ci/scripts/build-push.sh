f08edf01c5c4e2f302be0a4dee0fd8fd436166f0
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2019-04-12 11:24:08 -0700

    updated ci build-push - binary will be created in doker image now

--- ci/scripts/build-push.sh
+++ ci/scripts/build-push.sh
@@ -17,8 +17,7 @@ else
     echo " "
 fi
 
-echo "The goal is to create a binary and place in /dist directory with a Dockerfile"
-echo "The concourse pipeline will build and push the docker image to DockerHub"
+echo "The concourse pipeline will build and push the docker image to DockerHub via a DockerFile"
 echo " "
 echo "At start, you should be in a /tmp/build/xxxxx directory with two folders:"
 echo "   /hello-go-deploy-azure-vm"
@@ -32,50 +31,64 @@ echo "List whats in the current directory"
 ls -la
 echo " "
 
-echo "Setup the GOPATH based on current directory"
-export GOPATH=$PWD
+echo "Copy what you want to make binary of (i.e. go build) into dist"
+cp -r hello-go-deploy-azure-vm/example-01/* dist
 echo " "
 
-echo "Now we must move our code from the current directory ./hello-go-deploy-azure-vm to" 
-echo "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-azure-vm"
-mkdir -p src/github.com/JeffDeCola/
-cp -R ./hello-go-deploy-azure-vm src/github.com/JeffDeCola/.
-echo " "
+echo "Copy the Dockefile into dist"
+cp dist/build-push/Dockerfile dist
 
-echo "cd src/github.com/JeffDeCola/hello-go-deploy-azure-vm/example-01"
-cd src/github.com/JeffDeCola/hello-go-deploy-azure-vm/example-01
-echo " "
-
-echo "Check that you are set and everything is in the right place for go:"
-echo "gopath is: $GOPATH"
-echo "pwd is: $PWD"
-ls -la
-
-echo "Create a binary hello-go in /bin"
-go build -o bin/hello-go main.go
-echo ""
-
-echo "cd to the /dist directory"
-cd "$GOPATH/dist"
-echo " "
-
-echo "cp the binary into /dist"
-cp "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-azure-vm/example-01/bin/hello-go" .
-echo " "
-
-echo "cp the Dockerfile into /dist"
-cp "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-azure-vm/example-01/build-push/Dockerfile" .
-echo " "
-
-echo "Make it executable by all - chmod +x"
-chmod +x hello-go
-echo " "
-
-echo "List whats in the /dist directory"
+echo "List whats in /dist"
+cd dist
 ls -la
 echo " "
+
+#echo "Setup the GOPATH based on current directory"
+#export GOPATH=$PWD
+#echo " "
+
+#echo "Now we must move our code from the current directory ./hello-go-deploy-azure-vm to" 
+#echo "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-azure-vm"
+#mkdir -p src/github.com/JeffDeCola/
+#cp -R ./hello-go-deploy-azure-vm src/github.com/JeffDeCola/.
+#echo " "
+
+#echo "cd src/github.com/JeffDeCola/hello-go-deploy-azure-vm/example-01"
+#cd src/github.com/JeffDeCola/hello-go-deploy-azure-vm/example-01
+#echo " "
+
+#echo "Check that you are set and everything is in the right place for go:"
+#echo "gopath is: $GOPATH"
+#echo "pwd is: $PWD"
+#ls -la
+
+#echo "Create a binary hello-go in /bin"
+#go build -o bin/hello-go main.go
+#echo ""
+
+#echo "cd to the /dist directory"
+#cd "$GOPATH/dist"
+#echo " "
+
+#echo "cp the binary into /dist"
+#cp "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-azure-vm/example-01/bin/hello-go" .
+#echo " "
+
+#echo "cp the Dockerfile into /dist"
+#cp "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-azure-vm/example-01/build-push/Dockerfile" .
+#echo " "
+
+#echo "Make it executable by all - chmod +x"
+#chmod +x hello-go
+#echo " "
+
+#echo "List whats in the /dist directory"
+#ls -la
+#echo " "
 
 echo "The concourse pipeline will build and push the docker image to DockerHub"
+echo "Its funny because you are in a docker image (on concourse) that will build a docker image"
+echo "Its like the movie inception"
 echo " "
 
 echo "build-push.sh (END)"

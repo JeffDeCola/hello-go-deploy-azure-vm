0eb634d04511f100064bf7f4caa95963add8b031
Jeff DeCola (Jeffs-MBP) <jeffdecola@gmail.com>
2019-04-12 03:36:50 -0700

    updated Dockerfile and build-push.sh to build binary in docker image

--- example-01/build-push/build-push.sh
+++ example-01/build-push/build-push.sh
@@ -22,24 +22,34 @@ echo "cd up to code"
 cd ..
 echo " "
 
-echo "Create a binary hello-go in /bin"
-go build -o bin/hello-go main.go
-echo " "
+#echo "Create a binary hello-go in /bin"
+#go build -o bin/hello-go main.go
+#echo " "
 
-echo "Copy the binary in /build-push because docker needs it with Dockerfile"
-cp bin/hello-go build-push/.
-echo " "
+#echo "Copy the binary in /build-push because docker needs it with Dockerfile"
+#cp bin/hello-go build-push/.
+#echo " "
 
-echo "cd build-push"
-cd build-push
-echo " "
+#echo "cd build-push"
+#cd build-push
+#echo " "
 
 echo "Build your docker image from binary /bin/hello-go using /build-push/Dockerfile"
-docker build -t jeffdecola/hello-go-deploy-azure-vm .
+docker build -f build-push/Dockerfile -t jeffdecola/hello-go-deploy-azure-vm .
+echo " "
+
+echo "Image size"
+docker images jeffdecola/hello-go-deploy-azure-vm:latest
 echo " "
 
 echo "Assuming you are logged in, lets push your built docker image to DockerHub"
 docker push jeffdecola/hello-go-deploy-azure-vm
+echo " "
+
+echo "Useful commands:"
+echo "     docker run --name hello-go-deploy-azure-vm -dit jeffdecola/hello-go-deploy-azure-vm"
+echo "     docker exec -i -t hello-go-deploy-azure-vm /bin/bash"
+echo "     docker logs hello-go-deploy-azure-vm"
 echo " "
 
 echo "************************************************************************"
